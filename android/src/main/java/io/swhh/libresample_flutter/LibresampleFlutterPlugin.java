4c71f885e0d5208d57bde3d5e80d41e17b1b95f2
rmri <intonarumori@gmail.com>
2024-05-27 16:30:44 +0200

    cleanup

--- android/src/main/java/io/swhh/libresample_flutter/LibresampleFlutterPlugin.java
+++ android/src/main/java/io/swhh/libresample_flutter/LibresampleFlutterPlugin.java
@@ -16,20 +16,6 @@ public class LibresampleFlutterPlugin implements FlutterPlugin, MethodCallHandle
     channel.setMethodCallHandler(new LibresampleFlutterPlugin());
   }
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "libresample_flutter");
-    channel.setMethodCallHandler(new LibresampleFlutterPlugin());
-  }
-
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     result.notImplemented();
