5077dfd72c5e8bed94c354375e1fe2220b4fca84
rmri <intonarumori@gmail.com>
2024-12-30 13:47:39 +0100

    regenerated the android example app

---
+++ example/android/app/src/main/kotlin/io/swhh/libresample_flutter_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package io.swhh.libresample_flutter_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- ios/Classes/config.h
+++ ios/Classes/config.h
@@ -1,15 +1,1 @@
-//
-//  config.h
-//  Pods
-//
-//  Created by Richard Heap on 1/15/20.
-//
-
-#ifndef config_h
-#define config_h
-
-#define HAVE_INTTYPES_H 1
-
-#include <stdint.h>
-
-#endif /* config_h */
+../../common/config.h
